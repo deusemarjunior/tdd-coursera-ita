d8e17548307728bd24e63884608b5bcd9eef591a
Deusemar Junior <chaves.djr@gmail.com>
2020-04-05 14:26:42 -0300

    Segunda rodada de teste e refactor

--- pilha/src/main/kotlin/exemplo/Pilha.kt
+++ pilha/src/main/kotlin/exemplo/Pilha.kt
@@ -1,13 +1,27 @@
 package exemplo
 
+import java.util.*
+
 class Pilha {
 
+    private var elemento: String? = null
+    private var quantidade: Int = 0
+
     fun estaVazia(): Boolean {
-        return true
+        return (elemento == null);
     }
 
-    fun tamanho(): Long {
-        return 0
+    fun tamanho(): Int {
+        return quantidade
+    }
+
+    fun empilha(elemento: String) {
+        this.elemento = elemento
+        quantidade++
+    }
+
+    fun topo(): String? {
+        return elemento
     }
 
 }
--- pilha/src/test/kotlin/exemplo/PilhaTeste.kt
+++ pilha/src/test/kotlin/exemplo/PilhaTeste.kt
@@ -2,6 +2,7 @@ package exemplo
 
 import org.junit.Assert
 import org.junit.Test
+import kotlin.test.assertFalse
 
 class PilhaTeste {
 
@@ -10,5 +11,14 @@ class PilhaTeste {
         var pilha = Pilha()
         assert(pilha.estaVazia())
         Assert.assertEquals(0,pilha.tamanho())
+    }
+
+    @Test
+    fun empilhaUmElemento(){
+        var pilha = Pilha()
+        pilha.empilha("primeiro")
+        assertFalse(pilha.estaVazia())
+        Assert.assertEquals(1 ,pilha.tamanho())
+        Assert.assertEquals("primeiro",pilha.topo())
     }
 }
